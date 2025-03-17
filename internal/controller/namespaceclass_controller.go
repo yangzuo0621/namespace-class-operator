484dc1e85c6ac0009800c2ade75fd4740629f45f
Zuo Yang <zyang0508@163.com>
2025-03-17 23:10:39 +0800

    watch for namespace

--- internal/controller/namespaceclass_controller.go
+++ internal/controller/namespaceclass_controller.go
@@ -19,10 +19,18 @@ package controller
 import (
 	"context"
 
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	akuityiov1 "akuity.io/namespaceclass/api/v1"
 )
@@ -56,8 +64,74 @@ func (r *NamespaceClassReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *NamespaceClassReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	if err := mgr.GetFieldIndexer().IndexField(context.Background(), &akuityiov1.Networking{}, NamespaceClassOwnerKey, func(rawObj client.Object) []string {
+		// grab the Networking object, extract the owner...
+		job := rawObj.(*akuityiov1.Networking)
+		owner := metav1.GetControllerOf(job)
+		if owner == nil {
+			return nil
+		}
+		// ...make sure it's a Networking...
+		if owner.APIVersion != ApiGVStr || owner.Kind != "Networking" {
+			return nil
+		}
+
+		return []string{owner.Name}
+	}); err != nil {
+		return err
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&akuityiov1.NamespaceClass{}).
 		Named("namespaceclass").
+		Watches(&corev1.Namespace{}, handler.EnqueueRequestsFromMapFunc(r.watchNamespaceResource), builder.WithPredicates(NamespacePredicate)).
 		Complete(r)
+}
+
+var (
+	NamespaceClassOwnerKey = ".metadata.controller"
+	ApiGVStr               = akuityiov1.GroupVersion.String()
+)
+
+// NamespaceClassLabel is the label used to identify the namespace class that namespace resource refers to.
+const NamespaceClassLabel = "namespaceclass.akuity.io/name"
+
+var NamespacePredicate = predicate.Funcs{
+	CreateFunc: func(e event.CreateEvent) bool {
+		namespace, isNamespaceObject := e.Object.(*corev1.Namespace)
+		if !isNamespaceObject {
+			return false
+		}
+		_, ok := namespace.Labels[NamespaceClassLabel]
+		return ok
+	},
+	UpdateFunc: func(e event.UpdateEvent) bool {
+		oldNamespace, _ := e.ObjectOld.(*corev1.Namespace)
+		newNamespace, _ := e.ObjectNew.(*corev1.Namespace)
+
+		_, oldOk := oldNamespace.Labels[NamespaceClassLabel]
+		_, newOk := newNamespace.Labels[NamespaceClassLabel]
+
+		return oldOk || newOk
+	},
+	DeleteFunc: func(e event.DeleteEvent) bool {
+		return false
+	},
+}
+
+func (r *NamespaceClassReconciler) watchNamespaceResource(_ context.Context, a client.Object) []reconcile.Request {
+	namespace, isNamespaceObject := a.(*corev1.Namespace)
+	if !isNamespaceObject {
+		return nil
+	}
+	namespaceClass, ok := namespace.Labels[NamespaceClassLabel]
+	if !ok {
+		return nil
+	}
+	return []reconcile.Request{{
+		NamespacedName: types.NamespacedName{
+			Name:      namespaceClass,
+			Namespace: namespace.Namespace,
+		},
+	}}
 }

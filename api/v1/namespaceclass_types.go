105bf18ca02c26aa018fb89f622d2e4a4554616d
Zuo Yang <zyang0508@163.com>
2025-03-17 22:25:44 +0800

    modify crds

--- api/v1/namespaceclass_types.go
+++ api/v1/namespaceclass_types.go
@@ -28,8 +28,7 @@ type NamespaceClassSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of NamespaceClass. Edit namespaceclass_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Networking NetworkingSpec `json:"networking,omitempty"`
 }
 
 // NamespaceClassStatus defines the observed state of NamespaceClass.
--- api/v1/networking_types.go
+++ api/v1/networking_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1
 
 import (
+	networkv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -28,8 +29,12 @@ type NetworkingSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Networking. Edit networking_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	NetworkPolicies []NetworkingPolicy `json:"networkpolicies"`
+}
+
+type NetworkingPolicy struct {
+	Name string                      `json:"name"`
+	Spec networkv1.NetworkPolicySpec `json:"spec"`
 }
 
 // NetworkingStatus defines the observed state of Networking.
